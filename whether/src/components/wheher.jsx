b6acf8c098bcc87b371932b953c6892f7eba65ee
SandeepMarumudi <marumudisandeep@gmail.com>
2024-09-04 13:08:26 +0530

    mod

--- whether/src/components/wheher.jsx
+++ whether/src/components/wheher.jsx
@@ -1,9 +1,11 @@
 import { Component } from "react";
 import axios from "axios";
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm';
 
 class Whether extends Component {
   state = {
     response: null,
+    input:""
   };
   componentDidMount() {
     this.getLocation();
@@ -31,14 +33,37 @@ class Whether extends Component {
     });
     console.log(this.state.response);
   };
+  getdata=(e)=>{
+    e.preventDefault()
+    this.getinputdata(this.state.input)
+  }
+
+  getinputdata=async(input)=>{
+    const {data}= await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=f693296ac14bc5ee60ca74b64280291a`)
+    const {main}=data
+    this.setState({input:main})
+  }
+  datachange=(e)=>{
+    this.setState({
+      input:e.target.value
+    })
+  }
 
   render() {
     return (
       <>
+      {  <form onSubmit={this.getdata}>
+          <input type="text" placeholder="Enter location" value={this.state.input} onChange={this.datachange} ></input>
+          <input type="submit" ></input>
+      </form>
+      }
         <h1>Whether report</h1>
-        <h2>current locaion={this.state.response?.name}</h2>
+        <h2>current location={this.state.response?.name}</h2>
         <h3>max temperature={this.state.response?.main.temp_max}</h3>
         <h3>min temperature={this.state.response?.main.temp_min}</h3>
+
+        <h2>{this.state.input.temp}</h2>
+    
       </>
     );
   }
